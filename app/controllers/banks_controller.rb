859b03e2234afc6ccb79f8736a23222207a3433d
Daniel Ambrosio <daniel.ambrosio@gmail.com>
2015-07-03 00:34:00 -0300

    coloquei autenticaca por usuario e fechei os métodos publicos do controller bak

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,6 @@
 class BanksController < ApplicationController
-  before_action :set_bank, only: [:show, :edit, :update, :destroy]
+  before_action :set_bank, only: [:show]
+  before_action :authenticate_user!
 
   # GET /banks
   # GET /banks.json
@@ -13,53 +14,53 @@ class BanksController < ApplicationController
   end
 
   # GET /banks/new
-  def new
-    @bank = Bank.new
-  end
+  # def new
+  #   @bank = Bank.new
+  # end
 
-  # GET /banks/1/edit
-  def edit
-  end
+  # # GET /banks/1/edit
+  # def edit
+  # end
 
-  # POST /banks
-  # POST /banks.json
-  def create
-    @bank = Bank.new(bank_params)
+  # # POST /banks
+  # # POST /banks.json
+  # def create
+  #   @bank = Bank.new(bank_params)
 
-    respond_to do |format|
-      if @bank.save
-        format.html { redirect_to @bank, notice: 'Bank was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @bank }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @bank.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @bank.save
+  #       format.html { redirect_to @bank, notice: 'Bank was successfully created.' }
+  #       format.json { render action: 'show', status: :created, location: @bank }
+  #     else
+  #       format.html { render action: 'new' }
+  #       format.json { render json: @bank.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PATCH/PUT /banks/1
-  # PATCH/PUT /banks/1.json
-  def update
-    respond_to do |format|
-      if @bank.update(bank_params)
-        format.html { redirect_to @bank, notice: 'Bank was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @bank.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /banks/1
+  # # PATCH/PUT /banks/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @bank.update(bank_params)
+  #       format.html { redirect_to @bank, notice: 'Bank was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: 'edit' }
+  #       format.json { render json: @bank.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /banks/1
-  # DELETE /banks/1.json
-  def destroy
-    @bank.destroy
-    respond_to do |format|
-      format.html { redirect_to banks_url }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /banks/1
+  # # DELETE /banks/1.json
+  # def destroy
+  #   @bank.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to banks_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -69,6 +70,6 @@ class BanksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bank_params
-      params.require(:bank).permit(:name, :parsers)
+      params.require(:bank).permit(:name)
     end
 end
